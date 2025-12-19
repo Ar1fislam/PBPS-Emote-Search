7746124cecee273ca9363d8d1505914a149b5fd7
Arif <muhammad.ariful98@gmail.com>
2025-12-19 21:08:41 +0600

    run py fixed

--- run.py
+++ run.py
@@ -7,4 +7,5 @@ if sys.platform.startswith("win"):
 
 if __name__ == "__main__":
     # Keep reload=False for Playwright stability on Windows
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=False)
+    # uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
