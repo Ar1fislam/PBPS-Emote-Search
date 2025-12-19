0521de55e488e8c50de94b7c66c5f93a5630ee8f
Arif <muhammad.ariful98@gmail.com>
2025-12-19 20:19:53 +0600

    project deployed

---
+++ run.py
@@ -0,0 +1,10 @@
+import sys
+import asyncio
+import uvicorn
+
+if sys.platform.startswith("win"):
+    asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+
+if __name__ == "__main__":
+    # Keep reload=False for Playwright stability on Windows
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
